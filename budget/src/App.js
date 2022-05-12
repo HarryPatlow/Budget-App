b9f68ccdbdbdf098158520a06e95e157300f1a06
Harry Patlow <ghp.coding@gmail.com>
2022-05-12 09:20:23 +0530

    complete navbar design

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,19 +1,115 @@
 import './App.css';
-import React from 'react'
+import React, {useState} from 'react'
 import Dashboard from './components/Dashboard';
+import NewBudgets from './components/NewBudget';
+import AllBudgets from './components/AllBudgets';
+
 
 function App() {
-  var dash = document.querySelector("#dash");
-  var all = document.querySelector("#all");
-  var newbud = document.querySelector("#new");
+  // var dashmob = document.querySelector("#dash-mob");
+  // var allmob = document.querySelector("#all-mob");
+  // var newbudmob = document.querySelector("#new-mob");
 
-  dash.onclick = () => {
-    
-  }
-  return (
+  var [contentMob, setContentMob] = useState(<Dashboard/>)
+  var [content, setContent] = useState(<Dashboard/>)
+
+  var [DashActive, setDashActive] = useState("active")
+  var [NewActive, setNewActive] = useState("inactive")
+  var [AllActive, setAllActive] = useState("inactive")
+  return(
     <>
+    <div className="min-h-full">
+  <nav className="bg-gray-800">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center justify-between h-16">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            <img className="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"></img>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              
+              <button onClick={()=>{
+                setContent(<Dashboard/>)
+                setDashActive('active')
+                setNewActive('inactive')
+                setAllActive('inactive')
+                }
+              } className={DashActive==='active'? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"} aria-current="page">Dashboard</button>
+
+              <button onClick={()=>{
+                setContent(<NewBudgets/>)
+                setDashActive('inactive')
+                setNewActive('active')
+                setAllActive('inactive')
+                }
+              } className={NewActive==='active'? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"} >New Budget</button>
+
+              <button onClick={()=>{
+                setContent(<AllBudgets/>)
+                setDashActive('inactive')
+                setNewActive('inactive')
+                setAllActive('active')
+                }
+              } className={AllActive==='active'? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}>All Budgets</button>
+            </div>
+          </div>
+        </div>
+        <div className="-mr-2 flex md:hidden">
+
+          <button type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="block h-6 w-6" id="dropdown" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+
+            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    
+    <div id="mobile-menu" className="md:hidden">
+      <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3" id="menu">
+      <button onClick={()=>{
+        setContent(<Dashboard/>)
+        setDashActive('active')
+        setNewActive('inactive')
+        setAllActive('inactive')
+        }
+      } className={DashActive==='active'? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"} aria-current="page">Dashboard</button>
+
+      <button onClick={()=>{
+        setContent(<NewBudgets/>)
+        setDashActive('inactive')
+        setNewActive('active')
+        setAllActive('inactive')
+        }
+      } className={NewActive==='active'? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"} >New Budget</button>
+
+      <button onClick={()=>{
+        setContent(<AllBudgets/>)
+        setDashActive('inactive')
+        setNewActive('inactive')
+        setAllActive('active')
+        }
+      } className={AllActive==='active'? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}>All Budgets</button>
+      </div>
+    </div>
+  </nav>
+  <div className='md:hidden'>
+        { content }
+  </div>
+  <div className='hidden md:block'>
+        { content }
+  </div>
+</div>
     </>
-  );
+  )
 }
+
 
 export default App;
--- budget/src/components/NewBudget.js
+++ budget/src/components/NewBudget.js
@@ -5,7 +5,7 @@ function NewBudgets(){
         <>
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold text-gray-900">New Budgets</h1>
+                <h1 class="text-3xl font-bold text-gray-900">New Budget</h1>
                 </div>
             </header>
         </>
